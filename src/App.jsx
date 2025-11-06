d36859336948258e9ec4e213c67f9edd7cc557cf
Shourya <shouryasinha23@gmail.com>
2025-11-07 04:05:20 +0530

    basic preview

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import './App.css'
+import './App.css';
+import Intro from './components/Intro';
+import Translator from './components/Translator';
+import Footer from './components/Footer';
 
 function App() {
-
   return (
     <>
-      <div/>
-        
+      <Intro />
+      <div id="translator-section">
+        <Translator />
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
